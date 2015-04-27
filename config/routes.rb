a4cefa65896c615f33db14e6c29ed36b1138c1b9
NIKHAT BHAYANI <NIKHATALY@GMAIL.COM>
2015-04-27 15:12:15 -0400

    Worked on restaurant views and creating a new user

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  get 'users/new'
+  root 'restaurants#index'
 
-  get 'users/create'
+  resources :restaurants, only: [:index, :show]
+
+  resources :users, only: [:new, :create]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,44 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+[ "African",
+  "Asian",
+  "European",
+].each do |name|
+    Category.create(name: name)
+end
+
+categories = Category.all
+
+10.times do
+  Restaurant.create(name: Faker::Company.name, address: Faker::Address.street_address, phone: Faker::PhoneNumber.phone_number)
+
+
+
+# [
+#   "Dentist",
+#   "Chiropractor",
+#   "Family Physician",
+#   "Physiotherapist",
+#   "Phrenologist",
+# ].each do |name|
+#   Specialty.create(name: name)
+# end
+
+# specialties = Specialty.all
+
+# 10.times do
+#   Doctor.create(name: Faker::Name.name, opening_time: 9, closing_time: 17, specialty: specialties.sample)
+# end
+
+# 30.times do
+#   Patient.create(name: Faker::Name.name, email: Faker::Internet.email, password: "abcd")
+# end
+
+# docs = Doctor.all
+# pats = Patient.all
+
+# 30.times do
+#   Appointment.create(doctor: docs.sample, patient: pats.sample, time: rand(9..18), date: "2015-04-25")
+# end
